26593f34f48f11893ebd76212a9e5ee98c4cb7b5
Bizubani <bizubani@gmail.com>
2018-12-05 23:06:26 -0400

    Made program functional and more adaptive using recursive backtracking.

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
-import itertools as it
+def is_feasible_to_continue(perm):
+    i = len(perm) -1 #determine the last of the last placed queen.
+    for j in range(i): # cycle through array to ensure queens are not attacking each other.
+        if i - j == abs(perm[i] - perm[j]): # it is not feasible to continue if queens are attacking each other.
+            return False
+    return True
 
-def is_solution(permutation)
-    for(i1, i2) in it.combinations()
+def build_permutations(perm, n):
+    if len(perm) == n:
+        print (perm)
+        return
+
+    for k in range(n):
+        if k not in perm:
+            perm.append(k)
+
+            if is_feasible_to_continue(perm):
+                build_permutations(perm, n)
+
+            perm.pop()
+
+
+build_permutations(perm = [], n = 8)
